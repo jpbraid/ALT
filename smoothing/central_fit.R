9440ce46bfe4c5c306bc82f5acec6dfee510ba4d
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-10-29 02:33:30 +1100

    Update central_fit.R

--- smoothing/central_fit.R
+++ smoothing/central_fit.R
@@ -17,3 +17,25 @@ for (gender in c("M", "F")) {
   
   # do a full join and write to CSV
 }
+
+# note that i'm using the "b-spline" basis above; the previous version uses a custom-defined basis as below:
+custom_basis <- function(x, knots) {
+    B <- matrix(nrow = length(x), ncol = length(knots) + 4)
+    B <- tapply(X, 1, .do(-(knots)^3)) # etc
+    return(B)
+}
+
+# we can confirm that we get the same result as last time using this basis:
+min_age <- 3
+max_age <- 102
+initial_knots <- c(1, 2, 3, 4, 5) # initial knots used last time
+
+etr_2012 <- read_csv("etr_ForRM.csv") %>% filter(age %in% 3:102)
+final_knots <- knot_iterate(x = etr$age, y = etr$crude_mx, knots = initial_knots, basis = custom_basis)
+spline_mx <- eval_knots(final_knots)
+
+# etc
+
+
+
+
