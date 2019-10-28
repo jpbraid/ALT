c557b478ea6e4c7c24e3902dc89a89c60ed80072
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-10-29 02:13:18 +1100

    Update central_fit.R

--- smoothing/central_fit.R
+++ smoothing/central_fit.R
@@ -1,15 +1,18 @@
 library(tidyverse) 
 
 # knot_iterate <- function(x, y, knots) etc.
+# eval_knots
 
-# set up params
+# set up params; initial knots are user-defined and may require some tinkering with
 min_age <- 3
 max_age <- 102
-initial_knots_M <- c()
-initial_knots_F <- c()
+initial_knots_M <- c(10, 20, 30)
+initial_knots_F <- c(30, 40, 50)
 
-# load in raw mortality data
+# load in "raw" mortality data
 for (gender in c("M", "F")) {
   etr <- read_csv(sprintf("etr_ForR%s", gender)) %>% filter(age %in% min_age:max_age)
-  # initial_knots <- 
+  final_knots <- knot_iterate(x = etr$age, y = etr$crude_mx, knots = initial_knots) # etc
+  spline_mx <- eval_knots(final_knots)
+  # do a full join and write to CSV
 }
