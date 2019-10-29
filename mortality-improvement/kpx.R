a94b9fae5700f993ad31b484b403c7af759844a1
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-10-30 00:56:13 +1100

    Update kpx.R

--- mortality-improvement/kpx.R
+++ mortality-improvement/kpx.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 # load in qx data from all life tables and calculate corresponding px's
-qx_data <- read_csv("qx_all_M.csv") 
+qx_data <- read_csv("qx_all_interpolated_M.csv") 
 # maybe sort this by age
 max_age <- max(qx_data$age)
 qx_data <- qx_data %>% select(-age)
@@ -22,12 +22,13 @@ for (i in 2:nrow(px_data)) {
   k_bar_qx[[i]] <- kpx[[i]][-nrow(kpx[[i]]), ]*qx_data[-(1:(i - 1)), ]
 }
 
-# knowing k|q_x we can calculate the curtate future lifetime at various ages
-# (actually we can also use the kpx's for that, using a little probability trick, BUT...)
-# (k|q_x is good to know, because it tells us the distribution (pmf) of K_x)
+# knowing k|q_x we can calculate the curtate future lifetime at various ages^
 e_0 <- apply(k_bar_qx[[1]] * (0:max_age), 2, sum) + 0.5
 e_65 <- apply(k_bar_qx[[66]] * (65:max_age), 2, sum) + 0.5
 # NOTE: to calculate e_x for early life tables we'll need to manually impute the missing qx's for old ages (cf. the notes)
 
 # plot the distribution of K_0 and K_65 for various calendar years
 # (to be continued)
+
+# ^actually we can also use the kpx's for that, using a little probability trick, BUT...
+# k|q_x is good to know, because it tells us the distribution (pmf) of K_x
