61ab887398410b62108443944aa7109f788f89a8
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-10-29 22:19:27 +1100

    Update kpx.R

--- mortality-improvement/kpx.R
+++ mortality-improvement/kpx.R
@@ -3,28 +3,31 @@ library(tidyverse)
 qx_data <- read_csv("qx_all_M.csv") %>% select(-age) # maybe sort by age first
 px_data <- 1 - qx_data # note row i corresponds to age i - 1
 
+# calculate the survival probabilities kpx for each life table
 kpx <- vector(mode = "list", length = nrow(px_data))
 names(kpx) <- 1:nrow(px_data)
 for(i in 1:nrow(px_data)) {
   kpx[[i]] <- rbind(1, cumprod(px_data[i:nrow(px_data), ]))
 }
-# kpx[[i]] basically tells me k_p_(i-1) for k = 0, 1, ... 
+# kpx[[i]] = {k_p_(i-1)} for k = 0, 1, ... 
 
-# get the distribution of T_x via: P(T_x = k) = k|q_0 = kp0*q_k
+# now calculate k|q_x = kpx*q_k
 k_bar_qx <- vector(mode = "list", length = nrow(px_data))
 k_bar_qx[[1]] <- kpx[[1]][-nrow(kpx[[1]]), ]*qx_data
 for (i in 2:nrow(px_data)) {
   k_bar_qx[[i]] <- kpx[[i]][-nrow(kpx[[i]]), ]*qx_data[-(1:(i - 1)), ]
 }
 
+# knowing k|q_x we can calculate the curtate future lifetime at various ages
+# (actually we can also use the kpx's for that, using a little probability trick, BUT...)
+# (k|q_x is good to know, because it tells us the distribution (pmf) of K_x)
 e_0 <- apply(k_bar_qx[[1]] * (0:120), 2, sum) + 0.5
-# i can get e_65 etc: e_65 <- apply(k_bar_qx[[66]] * (65:120), 2, sum) + 0.5
-# etc!!
+e_65 <- apply(k_bar_qx[[66]] * (65:120), 2, sum) + 0.5
 
-# and plot these distro's
+# plot the distribution of K_0 and K_65 for various calendar years
+# (to be continued)
 
-
-# EXTENSION: interpolate (fill in the blanks)
+# INTERPOLATE QX DATA
 # SORT DATAFRAMES BY COLUMN FIRST! (safety)
 qx_data <- read_csv("qx_all_M.csv") %>% select(-age)
 years_in_data <- qx_data %>% names() %>% as.numeric %>% sort()
@@ -44,6 +47,9 @@ for (i in all_years) {
   if (!(i %in% years_in_data)) {
     j <- max(years_in_data[years_in_data < i])
     k <- min(years_in_data[years_in_data > i])
-    qx_all[, as.character(i)] <- qx_all[, as.character(i - 1)]  - (qx_all[, as.character(j)] - qx_all[, as.character(k)])/(k - j)
+    qx_all[, as.character(i)] <- qx_all[, as.character(i - 1)] - (qx_all[, as.character(j)] - qx_all[, as.character(k)])/(k - j)
   }
 }
+
+# i guess i should combine these two for loops into an if-else loop
+# then again, that would involve constantly reassigning values to the same column!!
