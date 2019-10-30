8fa21b342876bcdf37cd165d2157131f7aada1a9
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-10-30 21:24:04 +1100

    Update MI_factor_smoothing.R
    close paren

--- mortality-improvement/MI_factor_smoothing.R
+++ mortality-improvement/MI_factor_smoothing.R
@@ -7,11 +7,11 @@ gender <- "M"
 range <- "short"
 dof <- 30
 
-MI <- read_csv(sprintf("output/best_MI_fit_%s_%s.csv", gender, range)
+MI <- read_csv(sprintf("output/best_MI_fit_%s_%s.csv", gender, range))
 fit_SS <- smooth.spline(x = MI$age, y = MI$MI, df = dof)
-plot(MI$age, MI$MI*100)
-points(fit_SS$x, fit_SS$y*100, col = "orange", lwd = 2, type = "l")
-#plot(fit_SS$x, fit_SS$y*100, col = "orange", type = "l")
+plot(MI$age, MI$MI)
+points(fit_SS$x, fit_SS$y, col = "orange", lwd = 2, type = "l")
+#plot(fit_SS$x, fit_SS$y, col = "orange", type = "l")
 
 df <- data.frame(age = fit_SS$x, MI_SS = fit_SS$y)
 write.csv(df, sprintf("MI_SS_%s_%s.csv", gender, range), row.names = F)
