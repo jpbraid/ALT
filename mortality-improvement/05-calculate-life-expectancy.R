9356aa50cc77d057e8bf5ee29ba60da3cd15e8ef
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-11-01 10:16:52 +1100

    Update 05-calculate-life-expectancy.R

--- mortality-improvement/05-calculate-life-expectancy.R
+++ mortality-improvement/05-calculate-life-expectancy.R
@@ -1,16 +1,18 @@
 # calculate forecasted qx values based on chosen scenario, up to max_year
 forecast_qx <- function(gender, current_year, max_year, scenario = c(25, 125)) {
   # read in qx data for current year and the relevant MI factors
-  qx_current <- read_csv(sprintf("qx_all_interpolated_%s.csv", gender)) %>% 
+  qx_current <- read_csv(sprintf("qx_all_%s.csv", gender)) %>% 
     select(as.character(current_year)) %>% unlist()
-  MI_factors <- read_csv(sprintf("MI_smoothed_%s_%g.csv", gender, scenario)) %>% 
-    select(MI) %>% unlist()
+  MI_factors <- read_csv(sprintf("output/MI_smoothed_%s_%g.csv", gender, scenario)) %>% 
+    select(MI_SS) %>% unlist()
   
-  # if the max age is different in each data set, we have to use the smaller of the two
-  # actually i need to do something a bit more sophisticated here i think
-  max_age <- min(length(qx_current), length(MI_factors)) - 1
-  qx_current <- qx_current[1:(max_age + 1)]
-  MI_factors <- MI_factors[1:(max_age + 1)]
+  # compel the two vectors to have the same length
+  if (length(qx_current) > length(MI_factors)) {
+    MI_factors <- c(MI_factors, rep(0, length(qx_current) - length(MI_factors)))
+  } else {
+    qx_current <- qx_current[1:length(MI_factors)]
+  }
+  max_age <- length(qx_current) - 1
   
   # now build up the projected qx's by multiplying by the relevant MI factor
   qx_projected <- matrix(nrow = max_age + 1, ncol = max_year - current_year + 1)
@@ -24,7 +26,6 @@ forecast_qx <- function(gender, current_year, max_year, scenario = c(25, 125)) {
   
   return(qx_projected)
   #write.csv(qx_projected, sprintf("qx_projected_%s_%s.csv", gender, scenario), row.names = F)
-
 }
 
 # calculate period e_x 
@@ -39,16 +40,31 @@ period_ex <- function(gender, current_age, current_year, period_year, scenario =
   # do the standard cumprod() on px's to find kpx, then sum to get e_x
   kpx <- cumprod(period_px[(current_age + 1):length(period_px)])
   e_x <- sum(kpx)
+  print(e_x)
   return(e_x)
 }
 
 # calculate cohort e_x by: 
 # 1. calculating projected qx's, 2. getting the "diagonal" , 3. etc.
 cohort_ex <- function(gender, current_age, current_year, start_year, scenario = c(25, 125)) {
-  max_year <- start_year + max_age - current_age - 1 # ??
+  # the key step is working out the max_year; it basically depends on the start_year, current_age, and max_age
+  qx_current <- read_csv(sprintf("qx_all_%s.csv", gender)) %>% 
+    select(as.character(current_year)) %>% unlist()
+  MI_factors <- read_csv(sprintf("output/MI_smoothed_%s_%g.csv", gender, scenario)) %>% 
+    select(MI_SS) %>% unlist()
+  
+  max_age <- ifelse(length(qx_current) > length(MI_factors), length(qx_current) - 1, length(MI_factors) - 1)
+  max_year <- start_year + max_age - current_age
   qx_projected <- forecast_qx(gender = gender, current_year = current_year, max_year = max_year, scenario = scenario)
   
-  diagonal_qx <- diag(qx_projected) #sorta
+  # we want to look at qx's starting from the start_year and going up to the max_year
+  # also the rows of the qx submatrix need to start at the current age and go up to the max age
+  row_start <- current_age + 1
+  row_end <- max_age + 1
+  col_start <- start_year - current_year + 1
+  col_end <- max_year - current_year + 1
+  qx_submatrix <- as.matrix(qx_projected[row_start:row_end, col_start:col_end])
+  diagonal_qx <- diag(qx_submatrix)
   diagonal_px <- 1 - diagonal_qx
   diagonal_kpx <- cumprod(diagonal_px)
   
@@ -63,13 +79,17 @@ current_year <- 2016
 period_year <- 2060
 improvement_range <- 125
 
-# crank out the corresponding period e_x
-period_ex <- period_ex (gender = gender, current_year = current_year, period_year = period_year, scenario = improvement_range)
+# crank out the corresponding period e_x and cohort e_x
+period_ex <- period_ex (gender = gender, current_age = current_age, current_year = current_year, period_year = period_year, scenario = improvement_range)
+cohort_ex <- cohort_ex(gender = gender, current_age = current_age, current_year = current_year, start_year = period_year, scenario = improvement_range)
+
+
+
 
 
 ## this is all good, just need to think about:
-## 1. naming convention for MI files (prolly will have to change to MI_smoothed_M_125.csv etc)
-## 2. how to handle the "max_age" when calculating projected qx / period ex
-## 3. also the whole max_age thing for cohort ex
+## 1. naming convention for MI files: assumed MI_smoothed_M_125.csv
+## 2. how to work out max_age within cohort_ex function (WITHOUT just reading in the qx_all and MI_factors data again)
 ## oh there's also a subtlety in that the MI factors (line 6) should really be linked to the current_year 
 ## (since they change from LT to LT)
+## *sigh* rename column name from MI_SS to.. MI_smoothed or something
