4c88f82c10ce142059b807de737544159aa1a1d4
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-10-31 17:51:24 +1100

    Update MI_factor_smoothing.R

--- mortality-improvement/MI_factor_smoothing.R
+++ mortality-improvement/MI_factor_smoothing.R
@@ -6,12 +6,16 @@ library(tidyverse)
 gender <- "M"
 range <- "short"
 dof <- 30
+max_spline_age <- 100
 
 MI <- read_csv(sprintf("output/best_MI_fit_%s_%s.csv", gender, range))
 fit_SS <- smooth.spline(x = MI$age, y = MI$MI, df = dof)
+MI_predicted <- predict(fit_SS, x = (max_spline_age):105)$y
 plot(MI$age, MI$MI)
 points(fit_SS$x, fit_SS$y, col = "orange", lwd = 2, type = "l")
-#plot(fit_SS$x, fit_SS$y, col = "orange", type = "l")
+points((max_spline_age):105, MI_predicted, col = "orange", lwd = 2, type = "l")
+#plot(fit_SS$x, fit_SS$y*100, col = "orange", type = "l")
 
-df <- data.frame(age = fit_SS$x, MI_SS = fit_SS$y)
-write.csv(df, sprintf("output/MI_SS_%s_%s.csv", gender, range), row.names = F)
+df <- rbind(data.frame(age = fit_SS$x[1:max_spline_age], MI_SS = fit_SS$y[1:max_spline_age], dof = dof, spline_max = max_spline_age), 
+            data.frame(age = max_spline_age:105, MI_SS = MI_predicted, df = df, spline_max = max_spline_age))
+write.csv(df, sprintf("output/MI_SS_%s_%s.csv", gender, range), row.names = F) 
