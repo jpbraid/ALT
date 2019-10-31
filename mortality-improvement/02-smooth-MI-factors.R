1f06b7814d1b153532ad7dfe0c8e38548bb33b91
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-10-31 23:23:40 +1100

    Update 02-smooth-MI-factors.R

--- mortality-improvement/02-smooth-MI-factors.R
+++ mortality-improvement/02-smooth-MI-factors.R
@@ -14,7 +14,6 @@ MI_predicted <- predict(fit_SS, x = (max_spline_age):105)$y
 plot(MI$age, MI$MI)
 points(fit_SS$x, fit_SS$y, col = "orange", lwd = 2, type = "l")
 points((max_spline_age):105, MI_predicted, col = "orange", lwd = 2, type = "l")
-#plot(fit_SS$x, fit_SS$y*100, col = "orange", type = "l")
 
 df <- rbind(data.frame(age = fit_SS$x, MI_SS = fit_SS$y, dof = dof, spline_max = max_spline_age), 
             data.frame(age = max_spline_age:105, MI_SS = MI_predicted, df = df, spline_max = max_spline_age))
