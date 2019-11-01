f03b4e02a0cda5cd865c971d5d6aec4596279a1b
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-11-01 16:36:02 +1100

    Update 05-calculate-life-expectancy.R

--- mortality-improvement/05-calculate-life-expectancy.R
+++ mortality-improvement/05-calculate-life-expectancy.R
@@ -83,8 +83,91 @@ improvement_range <- 125
 period_ex <- period_ex (gender = gender, current_age = current_age, current_year = current_year, period_year = period_year, scenario = improvement_range)
 cohort_ex <- cohort_ex(gender = gender, current_age = current_age, current_year = current_year, start_year = period_year, scenario = improvement_range)
 
+max_year_analysis <- 2020
+max_age_analysis <- 4
+
+# TABLES
+
+analysis_years <- c(2016, seq(from = 2020, to = 2060, by = 10))
+analysis_ages <- c(0, 30, 65)
+
+df <- data.frame(gender = NA, improvement_range = NA, current_age = NA, start_year = NA, current_year = NA, e_x = NA, assumption = NA)
+
+for(gender in (c("M", "F"))) {
+  for (scenario in c(25, 125)) {
+    for (year in analysis_years) {
+      for (age in analysis_ages) {
+          for (assumption in c("cohort", "period")) {
+            if (assumption == "cohort") {
+              ex <- cohort_ex(gender = gender, current_age = age, current_year = current_year, start_year = year, scenario = scenario)
+
+            } else {
+              ex <- period_ex(gender = gender, current_age = age, current_year = current_year, period_year = year, scenario = scenario)
+            }
+            
+            new_row <- data.frame(gender = gender, improvement_range = scenario, current_age = age, start_year = year, 
+                                  current_year = current_year, e_x = ex, assumption = assumption)
+            df <- rbind(df, new_row)
+          }
+      }
+    }
+  }
+} 
+
+df %>% filter(complete.cases(df)) %>% 
+       mutate(life_exp = e_x + current_age) %>% 
+       write.csv("output/life_expectancy_tables.csv", row.names = F)
 
 
+# FIGURE 12
+
+analysis_years <- seq(from = 2016, to = 2066, by = 5)
+
+df <- data.frame(gender = NA, scenario = NA, current_year = NA, start_year = NA, current_age = NA, e_x = NA)
+
+for (gender in c("M", "F")) {
+  for (year in analysis_years) {
+    if (year > current_year) {
+      for (scenario in c(25, 125)) {
+        ex <- period_ex(gender = gender, current_age = 0, current_year = current_year, period_year = year, scenario = scenario)
+        new_row <- data.frame(gender = gender, scenario = scenario, current_year = current_year, start_year = year, current_age = 0, e_x = ex)
+        df <- rbind(df, new_row)
+      }
+    } else {
+      ex <- period_ex(gender = gender, current_age = 0, current_year = year, period_year = year, scenario = 25)
+      new_row <- data.frame(gender = gender, scenario = 0, current_year = current_year, start_year = year, current_age = 0, e_x = ex)
+      df <- rbind(df, new_row)
+    }
+  }
+}
+
+df <- df %>% mutate(life_exp = current_age + e_x) %>% filter(complete.cases(df))
+write.csv(df, "output/figure12.csv", row.names = F)
+
+
+# FIGURE 13
+
+df <- data.frame(gender = NA, scenario = NA, current_year = NA, current_age = NA, e_x = NA)
+
+for (gender in c("M", "F")) {
+  for (scenario in c(0, 25, 125)) {
+    for (age in 0:100) {
+      if (scenario == 0) {
+        ex <- period_ex(gender = gender, current_age = age, current_year = current_year, period_year = current_year, scenario = 25)
+      } else {
+        ex <- cohort_ex(gender = gender, current_age = age, current_year = current_year, start_year = current_year, scenario = scenario)
+      }
+      new_row <- data.frame(gender = gender, scenario = scenario, current_year = current_year, current_age = age, e_x = ex)
+      df <- rbind(df, new_row)
+    }
+  }
+}
+
+df <- df %>% mutate(life_exp = current_age + e_x) 
+df %>% filter(complete.cases(df)) %>% write.csv("output/figure13.csv", row.names = F)
+
+
+## FIGURE 14?
 
 
 ## this is all good, just need to think about:
