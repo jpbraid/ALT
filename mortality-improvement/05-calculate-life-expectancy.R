423ba805f74276d5271ce7a8e8a4f8040ce073b6
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-11-04 17:34:06 +1100

    Update 05-calculate-life-expectancy.R

--- mortality-improvement/05-calculate-life-expectancy.R
+++ mortality-improvement/05-calculate-life-expectancy.R
@@ -44,13 +44,15 @@ for (scenario in c(25, 125)) {
     qx <- forecast_qx(gender = gender, from_year = current_year, 
                       to_year = max_analysis_year + max_age, MI_year = current_year, 
                       scenario = scenario) 
-    write.csv(qx, sprintf("qx_projections_%s_%g.csv", gender, scenario), row.names = F)
+    #write.csv(qx, sprintf("qx_projections_%s_%g.csv", gender, scenario), row.names = F)
     qx_projections <- rbind(qx_projections, qx)
   }
 }
 qx_data_doubled <- rbind(qx_data, qx_data) %>% select(-"2016")
 qx_projections <- cbind(qx_data_doubled, qx_projections) %>% select(age, gender, scenario, everything())
 # BETTER: add gender to the forecast_qx() output; double the data according to scenario; do a full join on (gender, scenario)
+## TODO
+
 
 get_ex <- function(age, gender, year, scenario = c(25, 125), assumption = c("period", "cohort")) {
   if (assumption == "period") {
@@ -104,20 +106,31 @@ for (gender in c("M", "F")) {
   }
 }
 table_data <- table_data %>% filter(complete.cases(table_data)) %>% mutate(life_exp = current_age + e_x)
+write.csv(table_data, "output/life_expectancy_tables.csv", row.names = F)
 
 # figure 12 is pretty straightforward
 analysis_years <- seq(from = 2016, to = 2066, by = 5)
 
 projected_life_exp <- data.frame(gender = NA, analysis_year = NA, scenario = NA, e_x = NA)
+
 for (gender in c("M", "F")) {
-  for (scenario in c(25, 125)) {
-    for (year in analysis_years) {
-      e_x <- get_ex(age = 0, gender = gender, year = year, scenario = scenario, assumption = "period")
-      new_row <- data.frame(gender = gender, analysis_year = year, scenario = scenario, e_x = e_x)
+  for (year in analysis_years) {
+    if (year == 2016) {
+      e_x <- get_ex(age = 0, gender = gender, year = year, scenario = 25, assumption = "period")
+      new_row <- data.frame(gender = gender, analysis_year = year, scenario = 0, e_x = e_x)
       projected_life_exp <- rbind(projected_life_exp, new_row)
+    } else {
+        for (scenario in c(25, 125)) {
+          e_x <- get_ex(age = 0, gender = gender, year = year, scenario = scenario, assumption = "period")
+          new_row <- data.frame(gender = gender, analysis_year = year, scenario = scenario, e_x = e_x)
+          projected_life_exp <- rbind(projected_life_exp, new_row)
+        }
     }
   }
 }
+
+projected_life_exp <- projected_life_exp %>% filter(complete.cases(projected_life_exp))
+write.csv(projected_life_exp, "output/figure12.csv", row.names = F)
 
 # get the "past" ones as well
 
@@ -125,31 +138,43 @@ for (gender in c("M", "F")) {
 analysis_ages <- 0:100
 analysis_year <- current_year
 
-cohort_life_expectancy <- data.frame(gender = NA, age = NA, scenario = NA, assumption = NA, e_x = NA)
+cohort_life_expectancy <- data.frame(gender = NA, scenario = NA, current_year = NA, current_age = NA, e_x = NA)
+
 for (gender in c("M", "F")) {
   for (age in analysis_ages) {
-    for (scenario in c(25, 125)) {
-      for (assumption in c("period", "cohort")) {
+    for (assumption in c("period", "cohort")) {
+      if (assumption == "period") {
         e_x <- get_ex(age = age, gender = gender, year = analysis_year, scenario = scenario, assumption = assumption)
-        new_row <- data.frame(gender = gender, age = age, scenario = scenario, assumption = assumption, e_x = e_x)
+        new_row <- data.frame(gender = gender, scenario = 0, current_year = current_year, current_age = age, ex = e_x)
         cohort_life_expectancy <- rbind(cohort_life_expectancy, new_row)
+      } else {
+          for (scenario in c(25, 125)) {
+            e_x <- get_ex(age = age, gender = gender, year = analysis_year, scenario = scenario, assumption = assumption)
+            new_row <- data.frame(gender = gender, scenario = scenario, current_year = current_year, current_age = age, ex = e_x)
+            cohort_life_expectancy <- rbind(cohort_life_expectancy, new_row)
+          }
       }
     }
   }
 }
 
+cohort_life_expectancy <- cohort_life_expectancy %>% 
+  filter(complete.cases(cohort_life_expectancy)) %>%
+  mutate(life_exp = current_age + ex) %>% arrange(gender, scenario)
+write.csv(cohort_life_expectancy, "output/figure13.csv", row.names = F)
 
-# THEN do a kbarqx function
+# then do a kbar_qx function
 # we get the relevant qx values (for every age); then for each k find kbarqx...
 
 get_kbar_qx <- function(age, gender, year, scenario = c(25, 125), assumption = c("period", "cohort")) {
   if (assumption == "period") {
     qx <- qx_projections %>% filter(gender == UQ(gender), scenario == UQ(scenario)) %>% select(as.character(year)) %>% unlist()
+    max_age <- length(qx) - 1
+    qx <- qx[(age + 1):(max_age + 1)]
     px <- 1 - qx
     kpx <- cumprod(px)
     kbar_qx <- lag(kpx, default = 1) * qx
-    print(kbar_qx)
-    # then calculate as normal
+    return(kbar_qx)
   } else {
     # extract the relevant submatrix
     qx_submatrix <- qx_projections %>% filter(gender == UQ(gender), scenario == UQ(scenario)) %>% as.matrix()
@@ -157,27 +182,41 @@ get_kbar_qx <- function(age, gender, year, scenario = c(25, 125), assumption = c
     years <- year:(year + max_age - age) %>% as.character()
     qx_submatrix <- qx_submatrix %>% as.data.frame() %>% select(years) %>% as.matrix()
     qx_submatrix <- qx_submatrix[(age + 1):(max_age + 1), ]
-    print(qx_submatrix)
     qx <- diag(qx_submatrix) %>% as.numeric() %>% unlist()
     px <- 1 - qx
     kpx <- cumprod(px)
     kbar_qx <- lag(kpx, default = 1) * qx
-    print(kbar_qx)
+    return(kbar_qx)
   }
 }
 
-get_kbar_qx(age = 65, gender = "M", year = 2016, scenario = 25, assumption = "cohort")
+start_age <- 65
+
+kbar_qx_period <- get_kbar_qx(age = start_age, gender = "F", year = current_year, scenario = 25, assumption = "period")
+kbar_qx_cohort <- get_kbar_qx(age = start_age, gender = "F", year = current_year, scenario = 25, assumption = "cohort")
+kbar_qx <- cbind(kbar_qx_period, kbar_qx_cohort)
+write.csv(kbar_qx, "output/kbarqx_F.csv", row.names = F)
 
 
-## NOTE: assumption cohort is incompatible with analysis_year < current_year (at the moment)
-## if you want to do a cohort calculation for an earlier year, you need to change the current_year at the top, and recalculate the qx_projections
-## (because the projections are based on MI data **from the current year**)
-## hmm this is a bit subtle. do you WANT to calculate cohort life expectancy from 2011 USING the 2011 MI factors? or... would you rather use the
-## actual qx's from 2011 to 2016, and then use the 2016 MI factors to project qx's from there?? the latter seems to make more sense
-## other thing is i've tried to do this idea where you can grab multiple ex's at once by passing the function a list of years
-## but i don't think this works yet
+#################################
+### WARNING: MAJOR HACK AHEAD
+## (then again, the program kind of forces you to do this, since the functions refer to some global variables... not good!)
+#################################
 
-## PLAN: paste the MI_factors (2016+2011) together into a single long dataframe
-## run all the above (maybe write the qx_projections to csv)
-## then change MI_year to 2011 in the qx_projections, and calculate kbarqx there
-## then you're done
+current_year <- 2011
+qx_projections <- as.data.frame(matrix(nrow = 0, ncol = max_analysis_year + max_age - current_year + 1))
+names(qx_projections) <- as.character(current_year:(max_analysis_year + max_age))
+for (scenario in c(25, 125)) {
+  for (gender in c("M", "F")) {
+    qx <- forecast_qx(gender = gender, from_year = current_year + 5, 
+                      to_year = max_analysis_year + max_age, MI_year = current_year, 
+                      scenario = scenario) 
+    #write.csv(qx, sprintf("qx_projections_%s_%g.csv", gender, scenario), row.names = F)
+    qx_projections <- rbind(qx_projections, qx)
+  }
+}
+qx_data_doubled <- rbind(qx_data, qx_data) %>% select(-"2016")
+qx_projections <- cbind(qx_data_doubled, qx_projections) %>% select(age, gender, scenario, everything())
+
+kbar_qx_cohort_old <- get_kbar_qx(age = start_age, gender = "F", year = current_year + 5, scenario = 25, assumption = "cohort")
+write.csv(kbar_qx_cohort_old, "output/kbarqx_F_old_factors.csv")
