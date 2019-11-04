ba78c891874efca30817aacd5b9ca8bbbd58848e
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-11-05 01:12:36 +1100

    Update hyndman-ullah.R

--- coherent-mortality-forecasting/hyndman-ullah.R
+++ coherent-mortality-forecasting/hyndman-ullah.R
@@ -3,4 +3,13 @@ library(pcaPP)
 
 # pull in the raw mortality data from each year
 # note: this analysis is only for a single population, so we select e.g. just the male mortality rates
-mortality_data <- read_csv("qx_all.csv") %>% filter(gender == "M")
+mortality_data <- read_csv("mx_all.csv") %>% 
+                  filter(gender == "M") %>% 
+                  gather(key = "year", value = "m_x", -c(age, gender))
+
+# so here rather than for-ing through years we can use broom / apply logic to fit all the models at once
+for (year in unique(mortality_data$year)) {
+  mortality_subset <- mortality_data %>% filter(year == !!year) %>% mutate(variance = CETR/(m_x*(1 - m_x)))
+  # fit a weighted regression spline
+ }
+
