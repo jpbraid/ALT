c4c7b1c989fafba63615ebd1ad3fbafc7435f2a1
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-11-06 23:40:14 +1100

    Update hyndman-ullah.R

--- coherent-mortality-forecasting/hyndman-ullah.R
+++ coherent-mortality-forecasting/hyndman-ullah.R
@@ -7,6 +7,11 @@ mortality_data <- read_csv("mx_all.csv") %>%
                   filter(gender == "M") %>% 
                   gather(key = "year", value = "m_x", -c(age, gender))
 
+# look at the change in shape over the years
+mortality_data %>% ggplot(aes(x = age, y = m_x, colour = as.factor(year))) + 
+                    geom_line() + 
+                    labs(colour = "year")
+
 # so here rather than for-ing through years we can use broom / apply logic to fit all the models at once
 for (year in unique(mortality_data$year)) {
   mortality_subset <- mortality_data %>% filter(year == !!year) %>% mutate(variance = CETR/(m_x*(1 - m_x)))
